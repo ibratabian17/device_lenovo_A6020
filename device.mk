6647d3844dc19e8f1a1a2953038deffe34133491
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    A6020: Enable freeform windows
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: TTTT55 <iamtttt55@gmail.com>

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
