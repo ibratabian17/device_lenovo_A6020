9e8945993d27537c5c320b29972ce85726c58b29
Bruno Martins <bgcngm@gmail.com>
2020-09-29 10:45:23 +0100

    A6020: Build health HAL as per R requirements
    The devices fail to boot otherwise.
    
    Change-Id: Ia245ace574e3079d47287cd0e5ec908771851035

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service 
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common_system \
