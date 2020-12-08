8594c20c4607a1e10beb747a78c87d69c3219d78
Jeferson Oliveira <jroliveira.oliveira301@gmail.com>
2020-12-08 18:57:12 +0100

    A6020: Import relevant Android GO configs
    Change-Id: Ie6dc00094c6af8f561a5285daf74273a3e7d621d

--- device.mk
+++ device.mk
@@ -373,3 +373,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
+
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
