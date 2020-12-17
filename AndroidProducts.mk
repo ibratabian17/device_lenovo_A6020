72dd69629c4dcbbd4489a9a25a0933c00320d958
Jeferson Oliveira <jroliveira.oliveira301@gmail.com>
2020-12-17 14:38:00 +0100

    A6020: Back home
    Change-Id: I3142ac9eaf1c2ff0aa34b994406458a1854b2e6b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_A6020.mk
+    $(LOCAL_DIR)/lineage_A6020.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_A6020-userdebug \
-    arrow_A6020-eng \
-    arrow_A6020-user
+    lineage_A6020-userdebug \
+    lineage_A6020-eng \
+    lineage_A6020-user
