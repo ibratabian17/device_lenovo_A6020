94b6c3c88923de862f2997e41b896fa2d3e0ad5b
ibratabian17 <ibratabian@gmail.com>
2022-04-07 23:40:40 +0700

    A6020: fix messed lineage bring up

--- lineage_A6020.mk
+++ lineage_A6020.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk
 # Inherit from A6020 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product-if-exists, vendor/arrow/config/common.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -30,9 +30,8 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
-DEVICE_MAINTAINER := Ibratabian17
 PRODUCT_DEVICE := A6020
-PRODUCT_NAME := arrow_A6020
+PRODUCT_NAME := lineage_A6020
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Vibe K5
 PRODUCT_MANUFACTURER := Lenovo
